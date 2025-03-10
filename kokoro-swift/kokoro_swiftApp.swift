6110a0b6a3624b8f11530879b8b059d09016a43f
Ilia Sazonov <ilia_saz@yahoo.com>
2025-03-10 16:44:38 -0700

    Initial Commit

---
+++ kokoro-swift/kokoro_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  kokoro_swiftApp.swift
+//  kokoro-swift
+//
+//  Created by Ilia Sazonov on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct kokoro_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kokoro-swiftTests/kokoro_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  kokoro_swiftTests.swift
+//  kokoro-swiftTests
+//
+//  Created by Ilia Sazonov on 3/10/25.
+//
+
+import Testing
+@testable import kokoro_swift
+
+struct kokoro_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
